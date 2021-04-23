99c962e132de50e220355009880a3ce8964758ad
Ozzie <mikeosmonson@gmail.com>
2021-04-23 13:46:37 -0600

    Complete Modeling Using Hashes Model B

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+my_pet = {
+  "pet_type" => 'Dog',
+  "pet_name" => 'Falkor',
+  "age_in_years" => 4.2,
+  "years_of_frendship" => 420,
+  "flight_ability" => true,
+  "best_friend" => true,
+  "sightings" => ['AZ', 'NV', 'CO', 'Never Ending Story'],
+  "favorite_foods" => ['Bacon', 'Cheese', 'Pizza', 'Soup']
+}
