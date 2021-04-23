887256e6afa605a71f99df4337a5b5af54b19a45
Ozzie <mikeosmonson@gmail.com>
2021-04-23 13:36:00 -0600

    Complete Modeling Using Hashes model a challenge

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+mohave_higher_learning = {
+  "school_state" => 'Arizona',
+  "school_city" => 'Bullhead City',
+  "enrollment_headcount" => 1045,
+  "number_of_classes" => 201,
+  "junior_high" => false,
+  "high_school" => true,
+  "student_grade_levels" => ['Freshman', 'Sophmore', 'Junior', 'Senior'],
+  "sports_offered" => ['Football', 'Basket Ball', 'Soccer', 'Tennis'],
+}
