3ca68fd8f4be2b67fc454c3726781c284310045b
Ozzie <mikeosmonson@gmail.com>
2021-04-23 13:20:15 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Ozzie Osmonson"
+is_active = true
+famous_person = "Ozzie Osborn"
+num_3 = 27
+num_9 = 11
+sum = num_3 + num_9
+animals = ["Lions", "Tigers", "Bears"]
+animals.shift()
+famous_person.length
